d616a65f8f9a16696b592acbe966929106bbdb77
FireFox <FireFox2000000@gmail.com>
2018-04-06 20:17:50 +1000

    Apply inline

--- GbaGameEngine/src/engine/base/core/Memory.h
+++ GbaGameEngine/src/engine/base/core/Memory.h
@@ -2,7 +2,7 @@
 #include "string.h"
 #include "../Typedefs.h"
 
-static void MemCopy(const void* src, void* dest, u32 size)
+inline static void MemCopy(const void* src, void* dest, u32 size)
 {
 	memcpy(dest, src, size);
 }
