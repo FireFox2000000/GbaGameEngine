3bcd34fff99b69fb88a4cbfa759d643abc3e87ab
FireFox <FireFox2000000@gmail.com>
2018-04-06 19:57:12 +1000

    Iterator implementation.

--- GbaGameEngine/src/engine/base/core/stl/FixedArray.h
+++ GbaGameEngine/src/engine/base/core/stl/FixedArray.h
@@ -9,15 +9,32 @@ class FixedArray
 	T buffer[size];
 
 public:
+	typedef T* iterator;
+	typedef const T* const_iterator;
+
 	FixedArray();
 	FixedArray(const FixedArray<T, size> & that) { *this = that; }
 	~FixedArray() {}
 
+	inline u32 Count() { return size; }
 	inline u32 Count() const { return size; }
 	inline u32 Count() volatile { return size; }
 
-	inline void operator = (const FixedArray<T, size>& that) { MemCopy(&that, this, sizeof(T) * Count()); }
+	iterator begin() { return &buffer[0]; }
+	const_iterator begin() const { return &buffer[0]; }
 
+	iterator end() { return &buffer[Count()]; }
+	const_iterator end() const { return &buffer[Count()]; }
+
+	inline T & operator[](u32 index) { return buffer[index]; }
+	inline const T & operator[](u32 index) const { return buffer[index]; }
+	inline volatile T & operator[](u32 index) volatile { return buffer[index]; }
+	
+	inline T * At(u32 index) { return index < Count() ? &buffer[index] : NULL; }
+	inline const T * At(u32 index) const { return index < Count() ? &buffer[index] : NULL; }
+	inline volatile T * At(u32 index) volatile { return index < Count() ? &buffer[index] : NULL; }
+
+	inline void operator = (const FixedArray<T, size>& that) { MemCopy(&that, this, sizeof(T) * Count()); }
 	inline void operator = (const FixedArray<T, size>& that) volatile
 	{
 		for (u32 i = 0; i < size; ++i)
@@ -25,16 +42,7 @@ public:
 			buffer[i] = that.buffer[i];
 		}
 	}
-
-	inline T & operator[](u32 index) { return buffer[index]; }
-	inline const T & operator[](u32 index) const { return buffer[index]; }
-	inline volatile T & operator[](u32 index) volatile { return buffer[index]; }
-	
-	inline T * At(u32 index);
-	inline const T * At(u32 index) const;
-	inline volatile T * At(u32 index) volatile;
 };
-
 
 template<class T, u32 size>
 FixedArray<T, size>::FixedArray()
@@ -44,19 +52,4 @@ FixedArray<T, size>::FixedArray()
 	{
 		buffer[i] = T();
 	}
-}
-
-template<class T, u32 size>
-inline T* FixedArray<T, size>::At(u32 index) {
-	return index < Count() ? &buffer[index] : NULL;
-}
-
-template<class T, u32 size>
-inline volatile T* FixedArray<T, size>::At(u32 index) volatile {
-	return index < Count() ? &buffer[index] : NULL;
-}
-
-template<class T, u32 size>
-inline const T* FixedArray<T, size>::At(u32 index) const {
-	return index < Count() ? &buffer[index] : NULL;
 }
