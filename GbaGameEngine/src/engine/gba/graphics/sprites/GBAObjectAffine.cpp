7ca022a27fd3fa7a703c84c52ec3a22ea2ad3344
FireFox <FireFox2000000@gmail.com>
2018-04-18 19:55:24 +1000

    Add affine object class.

---
+++ GbaGameEngine/src/engine/gba/graphics/sprites/GBAObjectAffine.cpp
@@ -0,0 +1,5 @@
+#include "GBAObjectAffine.h"
+
+GBA::ObjectAffine::~ObjectAffine()
+{
+}
---
+++ GbaGameEngine/src/engine/gba/graphics/sprites/GBAObjectAffine.h
@@ -0,0 +1,27 @@
+#ifndef PRAGMA_ONCE_ENGINE_GBA_GRAPHICS_SPRITES_GBAOBJECTAFFINE_H
+#define PRAGMA_ONCE_ENGINE_GBA_GRAPHICS_SPRITES_GBAOBJECTAFFINE_H
+
+#include "../../../base/Typedefs.h"
+#include "../../../base/Macros.h"
+
+namespace GBA
+{
+	class ObjectAffine
+	{
+		u16 fill0[3];
+		s16 m_pa;
+		u16 fill1[3];
+		s16 m_pb;
+		u16 fill2[3];
+		s16 m_pc;
+		u16 fill3[3];
+		s16 m_pd;
+
+	public:
+		ObjectAffine() : m_pa(0), m_pb(0), m_pc(0), m_pd(0) {}
+		~ObjectAffine();
+
+	} ALIGN(4);
+}
+
+#endif
