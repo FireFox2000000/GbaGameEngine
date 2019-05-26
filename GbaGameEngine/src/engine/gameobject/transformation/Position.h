87152d63947615a1bcf6801bef22a2282fbf4f44
FireFox <FireFox2000000@gmail.com>
2019-05-26 19:58:32 +1000

    Removed use of floating point. Was slowing down sprite renderer calculations.

--- GbaGameEngine/src/engine/gameobject/transformation/Position.h
+++ GbaGameEngine/src/engine/gameobject/transformation/Position.h
@@ -1,9 +1,10 @@
 #pragma once
+#include "engine/base/core/stl/FixedPoint.h"
 #include "engine/math/Vector2.h"
 
 namespace Component
 {
-	struct Position : Vector2f
+	struct Position : Vector2<FixedPoint<int, 8>>
 	{
 	};
 }
