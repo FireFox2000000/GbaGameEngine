830a82728af90b5a888008f1def5277cc84df0ed
FireFox <FireFox2000000@gmail.com>
2020-01-17 09:50:46 +1100

    Gameobject can now be directly initialised from engine.

--- GbaGameEngine/src/engine/gameobject/GameObject.h
+++ GbaGameEngine/src/engine/gameobject/GameObject.h
@@ -2,9 +2,12 @@
 
 #include "engine/base/ecs/ManagedEntity.h"
 
+class Engine;
+
 // A managed entity that automatically adds transformation components
 class GameObject : public ECS::ManagedEntity
 {
 public:
 	GameObject(ECS::EntityComponentManager* ref);
+	GameObject(Engine* engine);
 };
