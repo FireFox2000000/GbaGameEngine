b3c82fe86ef9076977e07fc610298e45cb0c58c8
FireFox <FireFox2000000@gmail.com>
2020-01-19 16:26:11 +1100

    Queue up scene changes for extra safely when trying to change scenes whist doing updated for the same scene.

--- GbaGameEngine/src/engine/scene/SceneManager.cpp
+++ GbaGameEngine/src/engine/scene/SceneManager.cpp
@@ -8,5 +8,35 @@ SceneManager::SceneManager()
 
 SceneManager::~SceneManager()
 {
+	if (m_current)
+		delete m_current;
+
+	if (m_queuedScene)
+		delete m_queuedScene;
+}
+
+void SceneManager::UpdatedQueuedChange(Engine * engine)
+{
+	if (m_queuedScene)
+	{
+		if (m_current)
+		{
+			m_current->Exit(engine);
+			delete m_current;
+		}
+
+		m_current = m_queuedScene;
+		m_queuedScene = nullptr;
+
+		m_current->Enter(engine);
+	}
+}
+
+void SceneManager::UpdateScene(Engine * engine)
+{
+	UpdatedQueuedChange(engine);
+
+	if (m_current)
+		m_current->Update(engine);
 }
 
