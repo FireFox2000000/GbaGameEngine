40b0ea1f89490bfbeba2702d67fca0ea6b3d7478
FireFox <FireFox2000000@gmail.com>
2021-11-21 16:04:10 +1100

    Audio converter can now convert mp3 and wave files directly using NAudio instead of having to create the file externally using audacity.

--- GbaConversionTools/Tools/AudioConverter.cs
+++ GbaConversionTools/Tools/AudioConverter.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Collections.Generic;
+using NAudio.Wave;
+using System.Runtime;
 
 namespace GbaConversionTools.Tools
 {
@@ -12,36 +13,59 @@ namespace GbaConversionTools.Tools
     {
         public void Convert(string inputPath, string outputPath)
         {
-            using (BinaryReader reader = new BinaryReader(File.Open(inputPath, FileMode.Open)))
+            string ext = Path.GetExtension(inputPath);
+            if (ext == ".mp3")
             {
-                CppWriter cppWriter = new CppWriter(Path.GetFileName(Path.GetFileNameWithoutExtension(inputPath)), outputPath);
-
-                int sampleRate = 44100; // TODO, currently temp hardcoding. Eventually read from an audio file that has a proper header
-
-                // Write header information
-                cppWriter.Write(sampleRate);
-
-                Console.WriteLine("Processing audio samples");
-
-                List<byte> totalBytes = new List<byte>();
-
-                // Copy Signed 8-bit PCM data
-                while (reader.BaseStream.Position != reader.BaseStream.Length)
+                using (var reader = new Mp3FileReader(inputPath))
                 {
-                    totalBytes.Add(reader.ReadByte());
+                    WriteFromWaveStream(inputPath, outputPath, reader);
                 }
-
-                cppWriter.Write(totalBytes.Count);
-
-                foreach (byte b in totalBytes)
-                {
-                    cppWriter.Write(b);
-                }
-
-                Console.WriteLine("Audio file \"" + outputPath + "\" successfully converted");
-
-                cppWriter.Finalise();
             }
+            else if (ext == ".wav")
+            {
+                using (var reader = new WaveFileReader(inputPath))
+                {
+                    WriteFromWaveStream(inputPath, outputPath, reader);
+                }
+            }
+            else
+            {
+                throw new Exception("File format not supported.");
+            }
+        }
+
+        void WriteFromWaveStream(string inputPath, string outputPath, WaveStream reader)
+        {
+            var waveFormat = reader.WaveFormat;
+            int sampleRate = waveFormat.SampleRate;
+
+            using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
+            {
+                WaveFormat newFormat = new WaveFormat(sampleRate, 8, 1);
+                using (WaveStream eightBitPcmStream = new WaveFormatConversionStream(newFormat, pcmStream))
+                {
+                    CppWriter cppWriter = new CppWriter(Path.GetFileName(Path.GetFileNameWithoutExtension(inputPath)), outputPath);
+                    cppWriter.Write(sampleRate);
+
+                    List<byte> totalBytes = new List<byte>();
+                    int b = eightBitPcmStream.ReadByte();
+                    while (b != -1)
+                    {
+                        totalBytes.Add((byte)(b + 128));    // Make it signed 8-bit PCM, as this comes through as unsigned
+                        b = eightBitPcmStream.ReadByte();
+                    }
+
+                    cppWriter.Write(totalBytes.Count);
+
+                    foreach (byte data in totalBytes)
+                    {
+                        cppWriter.Write(data);
+                    }
+
+                    cppWriter.Finalise();
+                    Console.WriteLine("Audio file \"" + outputPath + "\" successfully converted");
+                }
+            }      
         }
     }
 }
