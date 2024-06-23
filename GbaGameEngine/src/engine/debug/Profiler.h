3f2e1f04e6ffab136a703d8b335ad58c3cae8677
Alexander "FireFox" Ong <FireFox2000000@gmail.com>
2024-06-23 18:31:57 +1000

    Profile out now automatically calculates correct cycle amount based on frequency.

--- GbaGameEngine/src/engine/debug/Profiler.h
+++ GbaGameEngine/src/engine/debug/Profiler.h
@@ -22,7 +22,7 @@ public:
 	{
 		auto profileStop = Time::CaptureSystemTimeSnapshot();
 		u32 profileResult = (profileStop.TotalCycles() - m_profileStart.TotalCycles()) * Time::ClockFreq;
-		Debug::LogFormat("[%s] = %d", m_label, profileResult);
+		Debug::LogFormat("[%s] = %d cycles", m_label, profileResult);
 	}
 };
 
@@ -38,16 +38,38 @@ public:
 		DEBUG_ASSERTMSG(!s_active, "Started precise profiler when another instance is already active. Only one instance of ScopedProfilerPrecise can be active at a time.");	
 		s_active = true;
 
-		auto& profilerClock = (*GBATEK::ioRegisterTimers)[GBATimerId::Profile];
+		volatile auto& profilerClock = (*GBATEK::ioRegisterTimers)[GBATimerId::Profile];
 		profilerClock.frequency = clockFrequency;
 		profilerClock.isEnabled = true;
 	}
 
 	~ScopedProfilerPrecise()
 	{
-		auto& profilerClock = (*GBATEK::ioRegisterTimers)[GBATimerId::Profile];
-		Debug::LogFormat("[%s] = %d", m_label, profilerClock.GetCurrentCount());
+		volatile auto& profilerClock = (*GBATEK::ioRegisterTimers)[GBATimerId::Profile];
+		auto currentCount = profilerClock.GetCurrentCount();
 		profilerClock.isEnabled = false;
+
+		switch (profilerClock.frequency)
+		{
+		case GBATEK::ClockFrequency::Cycle_64:
+		{
+			currentCount *= 64;
+			break;
+		}
+		case GBATEK::ClockFrequency::Cycle_256:
+		{
+			currentCount *= 256;
+			break;
+		}
+		case GBATEK::ClockFrequency::Cycle_1024:
+		{
+			currentCount *= 1024;
+			break;
+		}
+		default: break;
+		};
+
+		Debug::LogFormat("[%s] = %hu cycles", m_label, currentCount);
 		s_active = false;
 	}
 };
@@ -56,14 +78,41 @@ public:
 #define PROFILE_SCOPED_CLOCK_1(label) auto __scopeProfiler##label = ScopedProfilerPrecise(#label, GBATEK::ClockFrequency::Cycle_1)
 #define PROFILE_SCOPED_CLOCK_64(label) auto __scopeProfiler##label = ScopedProfilerPrecise(#label, GBATEK::ClockFrequency::Cycle_64)
 
-#define PROFILE_START(label) \
-		auto __profileStart##label = Time::CaptureSystemTimeSnapshot()
+#define PROFILE_START(label) auto __profileStart##label = Time::CaptureSystemTimeSnapshot()
 
 #define PROFILE_STOP(label) \
 { \
 	auto __profileStop##label = Time::CaptureSystemTimeSnapshot(); \
 	u32 profileResult = (__profileStop##label.TotalCycles() - __profileStart##label.TotalCycles()) * Time::ClockFreq; \
-	Debug::LogFormat("[%s] = %d", #label, profileResult); \
+	Debug::LogFormat("[%s] = %lu cycles", #label, profileResult); \
+}
+
+#define PROFILE_START_CLOCK_1(label) \
+	auto& profilerClock = (*GBATEK::ioRegisterTimers)[GBATimerId::Profile]; \
+	profilerClock.frequency = GBATEK::ClockFrequency::Cycle_1; \
+	profilerClock.isEnabled = true; \
+
+#define PROFILE_STOP_CLOCK_1(label) \
+{ \
+	auto& __profilerClock##label = (*GBATEK::ioRegisterTimers)[GBATimerId::Profile]; \
+	u16 currentCount = __profilerClock##label.GetCurrentCount();	\
+	__profilerClock##label.isEnabled = false; \
+	Debug::LogFormat("[%s] = %hu cycles", #label, currentCount); \
+}
+
+#define PROFILE_START_CLOCK_64(label) \
+{\
+	auto& __profilerClock##label = (*GBATEK::ioRegisterTimers)[GBATimerId::Profile]; \
+	__profilerClock##label.frequency = GBATEK::ClockFrequency::Cycle_64; \
+	__profilerClock##label.isEnabled = true; \
+}
+
+#define PROFILE_STOP_CLOCK_64(label) \
+{ \
+	auto& __profilerClock##label = (*GBATEK::ioRegisterTimers)[GBATimerId::Profile]; \
+	u16 currentCount = __profilerClock##label.GetCurrentCount();	\
+	__profilerClock##label.isEnabled = false; \
+	Debug::LogFormat("[%s] = %hu cycles", #label, currentCount * 64); \
 }
 
 #else
