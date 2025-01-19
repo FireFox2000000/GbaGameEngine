1b44b3002c4ea6a37055d308b11ce678f19674e8
Alexander "FireFox" Ong <FireFox2000000@gmail.com>
2025-01-19 15:12:36 +1100

    Put sin lut into anonamous namespace

--- GbaGameEngine/src/engine/math/Math.cpp
+++ GbaGameEngine/src/engine/math/Math.cpp
@@ -4,20 +4,23 @@
 #define SIN_LUT_SIZE 512
 #define M_PI 3.1415926535f
 
-// We calculate the sin LUT during compile time. Avoids having to link in a seperate file while still avoiding startup performance tank if this was done at run-time. 
-struct SinLookupTable {
-	s16 arr[SIN_LUT_SIZE];	// Stored as FixedPoint<u16, FRACTIONAL_BITS>, need primitive for constexpr
+namespace
+{
+	// We calculate the sin LUT during compile time. Avoids having to link in a seperate file while still avoiding startup performance tank if this was done at run-time. 
+	struct SinLookupTable {
+		s16 arr[SIN_LUT_SIZE];	// Stored as FixedPoint<u16, FRACTIONAL_BITS>, need primitive for constexpr
 
-	constexpr SinLookupTable() : arr()
-	{
-		for (int i = 0; i < SIN_LUT_SIZE; ++i)
+		constexpr SinLookupTable() : arr()
 		{
-			arr[i] = FixedPoint<s16, SIN_LUT_FRACTIONAL_BITS>::FloatCompress(sin(i * 2.f * M_PI / SIN_LUT_SIZE));
+			for (int i = 0; i < SIN_LUT_SIZE; ++i)
+			{
+				arr[i] = FixedPoint<s16, SIN_LUT_FRACTIONAL_BITS, s64>::FloatCompress(sin(i * 2.f * M_PI / SIN_LUT_SIZE));
+			}
 		}
-	}
-};
+	};
 
-constexpr SinLookupTable sinLut;
+	constexpr SinLookupTable sinLut;
+}
 
 int Math::Mod(int x, unsigned int N)
 {
